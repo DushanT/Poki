3131050375744755206c83413c1c1cad4892cb89
Dusan Tuzinsky <brooklynPD@gmail.com>
2021-05-11 11:44:03 +0200

    fix after update

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
-import { Scrollbars } from 'react-custom-scrollbars';
-import { CSSTransitionGroup as Transition } from 'react-transition-group';
+import React, { Component } from "react";
+import { Scrollbars } from "react-custom-scrollbars";
+import { CSSTransition } from "react-transition-group";
 
-const img_path = 'https://img.pokemondb.net/sprites/black-white/anim/';
+const img_path = "https://img.pokemondb.net/sprites/black-white/anim/";
 
 class Selection extends Component {
-
   handleClick(item) {
-    return ( e => {
+    return (e) => {
       this.props.handleSelect(item);
-    });
+    };
   }
 
   render() {
@@ -17,37 +16,39 @@ class Selection extends Component {
 
     return (
       <div>
-        <Transition
-          transitionName="fade"
-          transitionAppear={true}
-          transitionAppearTimeout={200}
-          transitionEnterTimeout={200}
-          transitionLeaveTimeout={200}>
-            {selected &&
-              <div className="overlay" 
-                   onClick={ this.handleClick(null) }></div>
-            }
-        </Transition>
-          
-        {selected &&
+        {selected && (
+          <CSSTransition in={selected} classNames="fade" timeout={200}>
+            <div className="overlay" onClick={this.handleClick(null)} />
+          </CSSTransition>
+        )}
+
+        {selected && (
           <div className="selection">
-            
-            <div className="selection__close" 
-                 onClick={ this.handleClick(null) }>x</div>
+            <div className="selection__close" onClick={this.handleClick(null)}>
+              x
+            </div>
 
             <Scrollbars autoHide>
               <div className="selection__image-wrapper">
-                <img className="selection__image" src={img_path + 'normal/' + selected.name + '.gif'} alt=""/>
-                <img className="selection__image" src={img_path + 'back-normal/' + selected.name + '.gif'} alt=""/>
+                <img
+                  className="selection__image"
+                  src={img_path + "normal/" + selected.name + ".gif"}
+                  alt=""
+                />
+                <img
+                  className="selection__image"
+                  src={img_path + "back-normal/" + selected.name + ".gif"}
+                  alt=""
+                />
               </div>
 
               <div className="details">
-                <div className="details__name" >
-                  {selected.name}
-                </div>
+                <div className="details__name">{selected.name}</div>
                 <div className="details__more">
                   <div>
-                    <span className="details__info-name">Base experience: </span> 
+                    <span className="details__info-name">
+                      Base experience:{" "}
+                    </span>
                     <span>{selected.base_experience}</span>
                   </div>
                   <div>
@@ -59,12 +60,10 @@ class Selection extends Component {
                     <span>{selected.weight}</span>
                   </div>
                   <div>
-                    <span className="details__info-name">Abilities: </span> 
+                    <span className="details__info-name">Abilities: </span>
                     <ul>
-                      {selected.abilities.map( item => (
-                        <li key={item.ability.name}>
-                          { item.ability.name }
-                        </li>
+                      {selected.abilities.map((item) => (
+                        <li key={item.ability.name}>{item.ability.name}</li>
                       ))}
                     </ul>
                   </div>
@@ -72,10 +71,9 @@ class Selection extends Component {
               </div>
             </Scrollbars>
           </div>
-        }
+        )}
       </div>
     );
   }
-
 }
 export default Selection;
